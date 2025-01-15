ea0f156756410195470b7b9cf934ac520d52dbaa
nfacundomendoza <n.facundomendoza@gmail.com>
2025-01-15 20:41:42 -0300

    estilo para formulario politraumatizado

--- forms/formulario_politraumatizado.py
+++ forms/formulario_politraumatizado.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired, InputRequired
 
 class FormularioPolitraumatizado(FlaskForm):
     nombre_paciente = TextAreaField('Nombre del Paciente', validators=[DataRequired(message="El nombre del paciente es obligatorio")])
-    sexo = SelectField('SEXO', choices=[('F', 'Femenino'), ('M', 'Masculino')])
+    sexo = SelectField('Sexo', choices=[('F', 'Femenino'), ('M', 'Masculino')])
 
     def get_default_date():
         return datetime.date.today()
@@ -13,90 +13,91 @@ class FormularioPolitraumatizado(FlaskForm):
     def get_default_time():
         return datetime.datetime.now().time()
 
-    fecha_evento = DateField('FECHA DEL EVENTO', format='%Y-%m-%d', default=get_default_date, validators=[DataRequired()])
-    hora = TimeField('HORA', format='%H:%M', default=get_default_time, validators=[DataRequired()])
+    fecha_evento = DateField('Fecha del Evento', format='%Y-%m-%d', default=get_default_date, validators=[DataRequired()])
+    hora = TimeField('Hora', format='%H:%M', default=get_default_time, validators=[DataRequired()])
 
     tipo_evento = SelectField(
-        'TIPO DE EVENTO', 
-        choices=[
-            ('', 'Seleccione...'),
-            ('NO INTENCIONAL (ACCIDENTE)', 'NO INTENCIONAL (ACCIDENTE)'),
-            ('INTENCIONAL: AGRESIÓN/VIOLENCIA', 'INTENCIONAL: AGRESIÓN/VIOLENCIA'),
-            ('INTENCIONAL: AUTOINFLIGIDA', 'INTENCIONAL: AUTOINFLIGIDA'),
-            ('SE DESCONOCE', 'SE DESCONOCE')
-        ]
+    'Tipo de evento', 
+    choices=[
+        ('', 'Seleccione...'),
+        ('NO INTENCIONAL (ACCIDENTE)', 'No intencional (accidente)'),
+        ('INTENCIONAL: AGRESIÓN/VIOLENCIA', 'Intencional: agresión/violencia'),
+        ('INTENCIONAL: AUTOINFLIGIDA', 'Intencional: autoinfligida'),
+        ('SE DESCONOCE', 'Se desconoce')
+    ]
     )
 
-    estado_general = TextAreaField('ESTADO GENERAL')
+    estado_general = TextAreaField('Estado general')
     temperatura = TextAreaField('Tº')
-    palidez = BooleanField('PALIDEZ')
-    cianosis = BooleanField('CIANOSIS')
-    edemas = BooleanField('EDEMAS')
-    lesiones_piel_partes_blandas = TextAreaField('LESIONES DE PIEL Y PARTES BLANDAS')
+    palidez = BooleanField('Palidez')
+    cianosis = BooleanField('Cianosis')
+    edemas = BooleanField('Edemas')
+    lesiones_piel_partes_blandas = TextAreaField('Lesiones de piel y partes blandas')
 
-    respiratorio = TextAreaField('RESPIRATORIO')
-    trauma_torax_cerrado = BooleanField('TRAUMA DE TORAX CERRADO')
-    fracturas_costales = BooleanField('FRACTURAS COSTALES')
-    contusiones = BooleanField('CONTUSIONES')
+    respiratorio = TextAreaField('Respiratorio')
+    trauma_torax_cerrado = BooleanField('Trauma de tórax cerrado')
+    fracturas_costales = BooleanField('Fracturas costales')
+    contusiones = BooleanField('Contusiones')
     arm = BooleanField('ARM')
-    sato2 = IntegerField('SATO2', default=0, validators=[InputRequired()])
-    pafi = IntegerField('PAFi', default=0, validators=[InputRequired()])
-    buena_entrada_aire_bilateral = BooleanField('BUENA ENTRADA DE AIRE BILATERAL')
+    sato2 = IntegerField('Sato2', default=0, validators=[InputRequired()])
+    pafi = IntegerField('Pafi', default=0, validators=[InputRequired()])
+    buena_entrada_aire_bilateral = BooleanField('Buena entrada de aire bilateral')
     fr = IntegerField('FR', default=0, validators=[InputRequired()])
-    hipoventilacion_derecha = BooleanField('HIPOVENTILACION DERECHA')
-    hipoventilacion_izquierda = BooleanField('HIPOVENTILACION IZQUIERDA')
-    neumotorax = BooleanField('NEUMOTORAX')
-    hemotorax = BooleanField('HEMOTORAX')
+    hipoventilacion_derecha = BooleanField('Hipoventilación derecha')
+    hipoventilacion_izquierda = BooleanField('Hipoventilación izquierda')
+    neumotorax = BooleanField('Neumotórax')
+    hemotorax = BooleanField('Hemotórax')
     tap = BooleanField('TAP')
-    circulatorio = TextAreaField('CIRCULATORIO')
+    circulatorio = TextAreaField('Circulatorio')
     ta = TextAreaField('TA')
     tam = TextAreaField('TAM')
     pvc = TextAreaField('PVC')
     fc = TextAreaField('FC')
-    relleno_capilar = BooleanField('RELLENO CAPILAR')
-    hemorragia_externa_activa = BooleanField('HEMORRAGIA EXTERNA ACTIVA')
-    sospecha_hemorragia_interna = BooleanField('SOSPECHA DE HEMORRAGIA INTERNA')
-    fallo_de_bomba = BooleanField('FALLO DE BOMBA')
-    derrame_pericardico = BooleanField('DERRAME PERICARDICO')
-    aceptable_perfusion_periferica = BooleanField('ACEPTABLE PERFUSION PERIFERICA')
-    shock = BooleanField('SHOCK')
-    inotropicos = BooleanField('INOTROPICOS')
-    requerimiento_de_transfusion = BooleanField('REQUERIMIENTO DE TRANSFUSION')
-    abdomen = TextAreaField('ABDOMEN')
-    trauma_abdominal_cerrado = BooleanField('TRAUMA ABDOMINAL CERRADO')
-    trauma_abdominal_abierto = BooleanField('TRAUMA ABDOMINAL ABIERTO')
-    blando_depresible_indoloro = BooleanField('BLANDO, DEPRESIBLE, INDOLORO')
+    relleno_capilar = BooleanField('Relleno capilar')
+    hemorragia_externa_activa = BooleanField('Hemorragia externa activa')
+    sospecha_hemorragia_interna = BooleanField('Sospecha de hemorragia interna')
+    fallo_de_bomba = BooleanField('Fallo de bomba')
+    derrame_pericardico = BooleanField('Derrame pericárdico')
+    aceptable_perfusion_periferica = BooleanField('Aceptable perfusión periférica')
+    shock = BooleanField('Shock')
+    inotropicos = BooleanField('Inotrópicos')
+    requerimiento_de_transfusion = BooleanField('Requerimiento de transfusión')
+    abdomen = TextAreaField('Abdomen')
+    trauma_abdominal_cerrado = BooleanField('Trauma abdominal cerrado')
+    trauma_abdominal_abierto = BooleanField('Trauma abdominal abierto')
+    blando_depresible_indoloro = BooleanField('Blando, depresible, indoloro')
     rha = TextAreaField('RHA')
-    distendido = BooleanField('DISTENDIDO')
+    distendido = BooleanField('Distendido')
     sng = BooleanField('SNG')
-    alim_enteral = BooleanField('ALIMENTACION ENTERAL')
+    alim_enteral = BooleanField('Alimentación enteral')
 
-    intervencion_quirurgica = BooleanField('INTERVENCION QUIRURGICA')
-    detalle_intervencion = TextAreaField('DETALLE DE INTERVENCION')
+    intervencion_quirurgica = BooleanField('Intervención quirúrgica')
+    detalle_intervencion = TextAreaField('Detalle de intervención')
 
-    drenajes = BooleanField('DRENAJES')
-    diuresis = TextAreaField('DIURESIS')
-    sonda_vesical = BooleanField('SONDA VESICAL')
-    funcion_neurologica = TextAreaField('FUNCION NEUROLOGICA')
+
+    drenajes = BooleanField('Drenajes')
+    diuresis = TextAreaField('Diuresis')
+    sonda_vesical = BooleanField('Sonda vesical')
+    funcion_neurologica = TextAreaField('Función neurológica')
     tec = BooleanField('TEC')
-    trauma_columna = BooleanField('TRAUMA COLUMNA')
-    bajo_sedanalgesia = BooleanField('BAJO SEDOANALGESIA')
-    glasgow = TextAreaField('GLASGOW')
-    pupilas = TextAreaField('PUPILAS')
-    foco_motor = TextAreaField('FOCO MOTOR')
+    trauma_columna = BooleanField('Trauma columna')
+    bajo_sedanalgesia = BooleanField('Bajo sedoanalgesia')
+    glasgow = TextAreaField('Glasgow')
+    pupilas = TextAreaField('Pupilas')
+    foco_motor = TextAreaField('Foco motor')
     pic = IntegerField('PIC', default=0, validators=[InputRequired()])
-    drenajes = BooleanField('DRENAJES')
+    drenajes = BooleanField('Drenajes')
 
-    collar_cervical = BooleanField('COLLAR CERVICAL')
-    inmovilizacion_tabla = BooleanField('INMOVILIZACIÓN CON TABLA')
+    collar_cervical = BooleanField('Collar cervical')
+    inmovilizacion_tabla = BooleanField('Inmovilización con tabla')
 
-    lesion_traumatologica = TextAreaField('LESION TRAMATOLÓGICA')
-    rts = IntegerField('REVISED TRAUMA SCORED', default=0, validators=[InputRequired()])
-    imagenes = TextAreaField('IMÁGENES')
-    laboratorio = TextAreaField('LABORATORIO')
-    interconsultas = TextAreaField('INTERCONSULTAS')
-    pronostico = TextAreaField('PRONÓSTICO')
-    apache_ii = TextAreaField('APACHE II')
+    lesion_traumatologica = TextAreaField('Lesión traumatológica')
+    rts = IntegerField('Revised trauma score', default=0, validators=[InputRequired()])
+    imagenes = TextAreaField('Imágenes')
+    laboratorio = TextAreaField('Laboratorio')
+    interconsultas = TextAreaField('Interconsultas')
+    pronostico = TextAreaField('Pronóstico')
+    apache_ii = TextAreaField('Apache II')
     sofa = TextAreaField('SOFA')
 
     # Botón para enviar el formulario
