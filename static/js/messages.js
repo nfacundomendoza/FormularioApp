f660a19a5582e9e8e3461633cb8bbc3e91feb98f
nfacundomendoza <n.facundomendoza@gmail.com>
2025-01-25 11:33:22 -0300

    agrego fade in

--- static/js/messages.js
+++ static/js/messages.js
@@ -1,10 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.alert').forEach(function (alert) {
+        alert.classList.add('fadein'); 
+        setTimeout(() => {
+            alert.classList.remove('fadein');
+        }, 9000); 
+    });
+
+  
     setTimeout(() => {
         document.querySelectorAll('.alert').forEach(function (alert) {
-            alert.classList.add('fadeout');
+            alert.classList.add('fadeout'); 
             setTimeout(() => {
-                alert.style.display = 'none';
-            }, 1000);
+                alert.style.display = 'none'; 
+            }, 1000); 
         });
-    }, 3000);
+    }, 16000); 
 });
